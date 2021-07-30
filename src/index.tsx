05a3fd6bc4cd42bdbc9d22ea44d2ba9365366d2e
Edward Matthews <matthews.edward.j@gmail.com>
2021-07-30 23:01:58 +0900

    Fixed line endings

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,9 @@ import ReactDOM from 'react-dom';
 import './custom.scss';
 import App from './App';
 
-
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root'),
 );
