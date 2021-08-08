0e5268d76c3b72c1243161ae3b220e7e57208d8d
Edward Matthews <matthews.edward.j@gmail.com>
2021-08-08 20:56:28 +0900

    Added loading spinner to articles

--- src/pages/Articles.tsx
+++ src/pages/Articles.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import Accordion from 'react-bootstrap/Accordion';
+import Spinner from 'react-bootstrap/Spinner';
 import moment from 'moment';
 
 const Articles: React.FC = () => {
@@ -17,27 +18,40 @@ const Articles: React.FC = () => {
         title: string;
     };
     const [articles, setArticles] = useState<ArticleT[]>([]);
-
+    const [articlesLoaded, setArticlesLoaded] = useState(false);
     useEffect(() => {
         fetch('https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@edward-matthews')
             .then((resp) => resp.json())
-            .then((blogs) => setArticles(blogs.items));
+            .then((blogs) => setArticles(blogs.items))
+            .then(() => setArticlesLoaded(true));
     }, []);
 
     return (
-        <Accordion defaultActiveKey="0" flush>
-            {articles.map((article, idx) => {
-                return (
-                    <Accordion.Item key={idx} eventKey={String(idx)}>
-                        <Accordion.Header>
-                            <span className="me-auto">{article.title}</span>
-                            <small className="ms-auto">{moment(article.pubDate).format('dddd, MMMM Do YYYY')}</small>
-                        </Accordion.Header>
-                        <Accordion.Body dangerouslySetInnerHTML={{ __html: article.description }}></Accordion.Body>
-                    </Accordion.Item>
-                );
-            })}
-        </Accordion>
+        <>
+            {articlesLoaded ? (
+                <Accordion defaultActiveKey="0" flush>
+                    {articles.map((article, idx) => {
+                        return (
+                            <Accordion.Item key={idx} eventKey={String(idx)}>
+                                <Accordion.Header>
+                                    <span className="me-auto">{article.title}</span>
+                                    <small className="ms-auto">
+                                        {moment(article.pubDate).format('dddd, MMMM Do YYYY')}
+                                    </small>
+                                </Accordion.Header>
+                                <Accordion.Body
+                                    dangerouslySetInnerHTML={{ __html: article.description }}
+                                ></Accordion.Body>
+                            </Accordion.Item>
+                        );
+                    })}
+                </Accordion>
+            ) : (
+                <Spinner animation="border" role="status" className="d-flex mx-auto mt-2">
+                    <span className="visually-hidden">Loading...</span>
+                </Spinner>
+            )}
+        </>
     );
 };
 
