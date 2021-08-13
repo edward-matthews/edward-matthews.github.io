350829b6ef091bac3657ad08b77cf8b4505e700d
Edward Matthews <matthews.edward.j@gmail.com>
2021-08-13 23:52:28 +0900

    Remove unnecessary class from div

--- src/components/FloatingButton.tsx
+++ src/components/FloatingButton.tsx
@@ -6,12 +6,12 @@ const FloatingButton: React.FC = () => {
     const [expanded, setExpanded] = useState(false);
 
     return (
-        <div className={`floating-options ${expanded && 'open'}`}>
+        <>
             <button className="floating-button" onClick={() => setExpanded(!expanded)}>
                 {expanded ? <i className="bi bi-dash" /> : <i className="bi bi-plus" />}
             </button>
             {expanded && <FloatingItems />}
-        </div>
+        </>
     );
 };
 
