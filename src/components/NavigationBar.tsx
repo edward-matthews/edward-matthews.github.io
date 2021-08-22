592eb605a42ac5941ab14e328d3824350b04395d
Edward Matthews <matthews.edward.j@gmail.com>
2021-08-22 09:59:55 +0900

    Optimize images

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -5,7 +5,10 @@ const NavigationBar: React.FC = () => {
     return (
         <header>
             <Link to="/">
-                <img alt="" src="/inkan3.png" />
+                <picture>
+                    <source srcSet="/logo.webp" type="image/webp" />
+                    <img src="/logo.png" />
+                </picture>
             </Link>
             <h1>Edward Matthews</h1>
         </header>
