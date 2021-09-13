6f40adfb9cc8f2b28acc467dd455d47fcb025df4
Edward Matthews <matthews.edward.j@gmail.com>
2021-09-13 11:26:57 +0100

    Amend colour scheme

--- src/components/MainBanner.tsx
+++ src/components/MainBanner.tsx
@@ -4,13 +4,15 @@ import { Link } from 'react-router-dom';
 const MainBanner: React.FC = () => {
     return (
         <header>
-            <Link to="/">
-                <picture>
-                    <source srcSet="/images/logo.webp" type="image/webp" />
-                    <img src="/images/logo.png" alt="A logo resembling a Japanese inkan stamp with the name えど" />
-                </picture>
-            </Link>
-            <h1>Edward Matthews</h1>
+            <div className="banner-container">
+                <Link to="/">
+                    <picture>
+                        <source srcSet="/images/logo.webp" type="image/webp" />
+                        <img src="/images/logo.png" alt="A logo resembling a Japanese inkan stamp with the name えど" />
+                    </picture>
+                </Link>
+                <h1 className="banner-text">Edward Matthews</h1>
+            </div>
         </header>
     );
 };
