66d403e2e08a611118e05155111418865ff962cf
Edward Matthews <matthews.edward.j@gmail.com>
2021-09-14 12:20:20 +0100

    Readd navbar

--- src/components/MainBanner.tsx
+++ src/components/MainBanner.tsx
@@ -1,19 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
 
 const MainBanner: React.FC = () => {
     return (
-        <header>
-            <div className="banner-container">
+        <>
+            <header>
                 <Link to="/">
                     <picture>
                         <source srcSet="/images/logo.webp" type="image/webp" />
                         <img src="/images/logo.png" alt="A logo resembling a Japanese inkan stamp with the name えど" />
                     </picture>
                 </Link>
-                <h1 className="banner-text">Edward Matthews</h1>
-            </div>
-        </header>
+            </header>
+            <Navbar bg="secondary" variant="light">
+                <Container>
+                    <Nav className="mx-auto">
+                        <Nav.Link href="/portfolio">Portfolio</Nav.Link>
+                        <Nav.Link href="/articles">Articles</Nav.Link>
+                        <Nav.Link href="/contact">Contact</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+        </>
     );
 };
 
